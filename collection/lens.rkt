89185cb69878afdcffbd3a81161619a9997a6e32
Alexis King <lexi.lambda@gmail.com>
2015-07-18 11:18:13 -0700

    Add default value support for ref-lens

--- collection/lens.rkt
+++ collection/lens.rkt
@@ -8,12 +8,17 @@
                   scribble/manual
                   "lens/private/sandbox.rkt"))
 
-(provide @proc-doc/names[ref-lens (any/c . -> . lens?) (key)
+(provide @proc-doc/names[ref-lens
+                         (case-> (any/c . -> . lens?)
+                                 (any/c any/c . -> . lens?))
+                         ((key) (key default))
                          @{Constructs a lens that operates on @racket[indexable?] instances.
                            @(lens:examples
                              (lens-transform (ref-lens 'key)
                                              (hash 'key 2)
-                                             (λ (x) (* 10 x))))}]
+                                             (λ (x) (* 10 x)))
+                             (lens-view (ref-lens 'key 'default)
+                                        (hash)))}]
          @proc-doc/names[nth-lens (exact-nonnegative-integer? . -> . lens?) (index)
                          @{Constructs a lens that operates on @racket[sequence?] instances.
                            @(lens:examples
@@ -21,9 +26,14 @@
                                              '(1 2 3)
                                              (λ (x) (* 10 x))))}])
 
-(define (ref-lens key)
-  (make-lens (λ (target) (ref target key))
-             (λ (target value) (set-ref target key value))))
+(define ref-lens
+  (case-lambda
+    [(key)
+     (make-lens (λ (target) (ref target key))
+                (λ (target value) (set-ref target key value)))]
+    [(key default)
+     (make-lens (λ (target) (ref target key default))
+                (λ (target value) (set-ref target key value)))]))
 
 (define (nth-lens index)
   (make-lens (λ (target) (nth target index))
@@ -34,7 +44,9 @@
   (test-case
    "ref-lens"
    (check-equal? (lens-transform (ref-lens 'key) (hash 'key 2) #{* 10})
-                 (hash 'key 20)))
+                 (hash 'key 20))
+   (check-equal? (lens-view (ref-lens 'key 'default) (hash))
+                 'default))
   (test-case
    "nth-lens"
    (check-equal? (lens-transform (nth-lens 1) '(1 2 3) #{* 10})
